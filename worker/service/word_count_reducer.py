7c00d1dbfb7908b8a3bdecaa0656148188be5098
Prasanna Nikhil Sathwik Vadlamani <sathwik@Prasannas-MacBook-Pro.local>
2023-04-16 21:36:12 -0400

    resolved the key_mapping issue

--- worker/service/word_count_reducer.py
+++ worker/service/word_count_reducer.py
@@ -2,29 +2,31 @@ import os
 
 class WordCountReducer:
 
-    def __init__(self, worker_no, file_path, key_mapping):
+    def __init__(self, worker_no, file_path, num_workers=3):
         self.file_path = file_path
         self.feature = "WordCount"
         self.worker_no = worker_no
-        self.key_mapping = key_mapping
+        #self.key_mapping = key_mapping
+        self.num_workers = num_workers
 
     def run(self):
+        local_key_mapping = {}
         try:
-            worker_no = int(os.getenv('workerNo'))
             with open(self.file_path, 'r') as file:
                 for line in file:
                     words = line.split(',')
                     key = words[0]
                     value = int(words[1])
 
-                    worker = abs(hash(key)) % 3 + 1
-                    if worker == worker_no:
-                        if key in self.key_mapping:
-                            self.key_mapping[key] += 1
+                    worker = abs(hash(key)) % self.num_workers + 1
+                    print(f"Key: {key}, Value: {value}, Worker: {worker}, Current Worker: {self.worker_no}")
+                    if worker == self.worker_no:
+                        if key in local_key_mapping:
+                            local_key_mapping[key] += 1
                         else:
-                            self.key_mapping[key] = 1
-
+                            local_key_mapping[key] = 1
         except Exception as e:
             print(e)
 
-        return "Process Completed"
+        return local_key_mapping
+
