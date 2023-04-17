21dadc5ef2983f2ecfdefd9f0fe05548c95d0757
Prasanna Nikhil Sathwik Vadlamani <sathwik@Prasannas-MacBook-Pro.local>
2023-04-16 21:34:54 -0400

    resolved the key_mapping issue

--- worker/service/reverse_web_link_reducer.py
+++ worker/service/reverse_web_link_reducer.py
@@ -4,30 +4,30 @@ from collections import defaultdict
 
 class ReverseWebLinkReducer:
 
-    def __init__(self, worker_no, file_path, key_mapping):
+    def __init__(self, worker_no, file_path, key_mapping, num_workers=3):
         self.file_path = file_path
-        self.feature = "ReverseWebLinkReducer"
+        self.feature = "WordCount"
         self.worker_no = worker_no
         self.key_mapping = key_mapping
+        self.num_workers = num_workers
+
 
     def run(self):
-        try:
-            worker_no = int(os.getenv('workerNo'))
-            with open(self.file_path, 'r') as file:
-                for line in file:
-                    # line = <target, source>
-                    words = line.split(',')
-                    key = words[0]
-                    value = words[1].strip()
+            local_key_mapping = {}
+            try:
+                with open(self.file_path, 'r') as file:
+                    for line in file:
+                        words = line.split(',')
+                        key = words[0]
+                        value = int(words[1])
 
-                    worker = abs(hash(key)) % 3 + 1
-                    if worker == worker_no:
-                        if key in self.key_mapping:
-                            self.key_mapping[key].append(value)
-                        else:
-                            self.key_mapping[key] = [value]
+                        worker = abs(hash(key)) % self.num_workers + 1
+                        if worker == self.worker_no:
+                            if key in local_key_mapping:
+                                local_key_mapping[key] += 1
+                            else:
+                                local_key_mapping[key] = 1
+            except Exception as e:
+                print(e)
 
-        except Exception as e:
-            print(e)
-
-        return "Process Completed"
+            return local_key_mapping
