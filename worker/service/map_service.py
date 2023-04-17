b734ea2ca6d752729b6ecce37a8a98fb426892d9
Prasanna Nikhil Sathwik Vadlamani <sathwik@Prasannas-MacBook-Pro.local>
2023-04-16 21:30:57 -0400

    modified regex in teh word_count() method to better read the input

--- worker/service/map_service.py
+++ worker/service/map_service.py
@@ -2,6 +2,7 @@ import os
 import re
 import random
 
+
 class MapService:
 
     def __init__(self, worker_no):
@@ -13,22 +14,26 @@ class MapService:
         end_line_no = map_obj['endNo']
         file_path_intermediate = f"intermediateFile-{self.worker_no}.txt"
 
-        with open(input_file, 'r') as file, open(file_path_intermediate, 'w') as intermediate_file_writer:
-            for _ in range(start_line_no):
-                next(file)
+        try:
+            with open(input_file, 'r') as file, open(file_path_intermediate, 'w') as intermediate_file_writer:
+                for _ in range(start_line_no):
+                    next(file)
+                count = start_line_no
+                for line in file:
+                    if count >= end_line_no:
+                        break
+                    line = re.sub(r'[^\w\s]', '', line)
+                    line = re.sub(r'\s+', ' ', line)
 
-            count = start_line_no
-            for line in file:
-                if count >= end_line_no:
-                    break
-                line = re.sub(r'\p{Punct}', '', line)
-                line = re.sub(r'\s+', ' ', line)
+                    words = line.split()
 
-                words = line.split()
+                    for word in words:
+                        intermediate_file_writer.write(f"{word.lower()},1\n")
+                    count += 1
 
-                for word in words:
-                    intermediate_file_writer.write(f"{word.lower()},1\n")
-                count += 1
+        except Exception as e:
+            print(e)
+            raise
 
         return file_path_intermediate
 
@@ -39,17 +44,22 @@ class MapService:
         pattern = map_obj['misc']
         file_path_intermediate = f"intermediateFile-{self.worker_no}.txt"
 
-        with open(input_file, 'r') as file, open(file_path_intermediate, 'w') as intermediate_file_writer:
-            for _ in range(start_line_no):
-                next(file)
+        try:
+            with open(input_file, 'r') as file, open(file_path_intermediate, 'w') as intermediate_file_writer:
+                for _ in range(start_line_no):
+                    next(file)
 
-            count = start_line_no
-            for line in file:
-                if count >= end_line_no:
-                    break
-                if pattern in line:
-                    intermediate_file_writer.write(f"{count + 1},{line.strip()}\n")
-                count += 1
+                count = start_line_no
+                for line in file:
+                    if count >= end_line_no:
+                        break
+                    if pattern in line:
+                        intermediate_file_writer.write(
+                            f"{count + 1},{line.strip()}\n")
+                    count += 1
+        except Exception as e:
+            print(e)
+            raise
 
         return file_path_intermediate
 
@@ -59,16 +69,20 @@ class MapService:
         end_line_no = map_obj['endNo']
         file_path_intermediate = f"intermediateFile-{self.worker_no}.txt"
 
-        with open(input_file, 'r') as file, open(file_path_intermediate, 'w') as intermediate_file_writer:
-            for _ in range(start_line_no):
-                next(file)
+        try:
+            with open(input_file, 'r') as file, open(file_path_intermediate, 'w') as intermediate_file_writer:
+                for _ in range(start_line_no):
+                    next(file)
 
-            count = start_line_no
-            for line in file:
-                if count >= end_line_no:
-                    break
-                words = line.strip().split('->')
-                intermediate_file_writer.write(f"{words[1]},{words[0]}\n")
-                count += 1
+                count = start_line_no
+                for line in file:
+                    if count >= end_line_no:
+                        break
+                    words = line.strip().split('->')
+                    intermediate_file_writer.write(f"{words[1]},{words[0]}\n")
+                    count += 1
+        except Exception as e:
+            print(e)
+            raise
 
         return file_path_intermediate
