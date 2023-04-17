82fd0e42c91ac78d09e62b93bbee550a21d2008b
Prasanna Nikhil Sathwik Vadlamani <sathwik@Prasannas-MacBook-Pro.local>
2023-04-16 21:18:32 -0400

    Update master_controller to handle POST requests

--- master/controller/master_controller.py
+++ master/controller/master_controller.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, jsonify
 from service.master_service import MasterService
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 service = MasterService()
 
 
