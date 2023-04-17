eec48a647b4e5b547b64f64ecfbdbba62a460b32
Prasanna Nikhil Sathwik Vadlamani <sathwik@Prasannas-MacBook-Pro.local>
2023-04-16 21:33:12 -0400

    resolved the ThreadPoolExecutor issue

--- worker/service/reduce_service.py
+++ worker/service/reduce_service.py
@@ -12,38 +12,56 @@ class ReduceService:
     def word_count(self, reduce_obj):
         key_mapping = {}
         intermediate_files = reduce_obj['intermediateFiles']
-        len_intermediate_files = len(intermediate_files)
+        num_workers = len(intermediate_files)
 
-        with ThreadPoolExecutor(max_workers=len_intermediate_files) as executor:
-            futures = [executor.submit(WordCountReducer(self.worker_no, file_path, key_mapping).run) for file_path in intermediate_files]
+        with ThreadPoolExecutor(max_workers=num_workers) as executor:
+            futures = [executor.submit(WordCountReducer(self.worker_no, file_path, num_workers).run) for file_path in intermediate_files]
+            print(f"Starting to wait for the reducer futures...")
 
             for future in as_completed(futures):
-                future.result()
+                local_key_mapping = future.result()
+                print(f"Local key mapping: {local_key_mapping}")
+                for key, value in local_key_mapping.items():
+                    if key in key_mapping:
+                        key_mapping[key] += value
+                    else:
+                        key_mapping[key] = value
 
+        print(f"Final key mapping: {key_mapping}")
         return FileOperator.write_hashmap_to_file(key_mapping)
 
     def distributed_grep(self, reduce_obj):
         key_mapping = {}
         intermediate_files = reduce_obj['intermediateFiles']
-        len_intermediate_files = len(intermediate_files)
+        num_workers = len(intermediate_files)
 
-        with ThreadPoolExecutor(max_workers=len_intermediate_files) as executor:
-            futures = [executor.submit(DistributedGrepReducer(self.worker_no, file_path, key_mapping).run) for file_path in intermediate_files]
+        with ThreadPoolExecutor(max_workers=num_workers) as executor:
+            futures = [executor.submit(DistributedGrepReducer(self.worker_no, file_path, num_workers).run) for file_path in intermediate_files]
 
             for future in as_completed(futures):
-                future.result()
+                local_key_mapping = future.result()
+                for key, value in local_key_mapping.items():
+                    if key in key_mapping:
+                        key_mapping[key] += value
+                    else:
+                        key_mapping[key] = value
 
         return FileOperator.write_hashmap_to_file(key_mapping)
 
     def reverse_web_link(self, reduce_obj):
         key_mapping = {}
         intermediate_files = reduce_obj['intermediateFiles']
-        len_intermediate_files = len(intermediate_files)
+        num_workers = len(intermediate_files)
 
-        with ThreadPoolExecutor(max_workers=len_intermediate_files) as executor:
-            futures = [executor.submit(ReverseWebLinkReducer(self.worker_no, file_path, key_mapping).run) for file_path in intermediate_files]
+        with ThreadPoolExecutor(max_workers=num_workers) as executor:
+            futures = [executor.submit(ReverseWebLinkReducer(self.worker_no, file_path, num_workers).run) for file_path in intermediate_files]
 
             for future in as_completed(futures):
-                future.result()
+                local_key_mapping = future.result()
+                for key, value in local_key_mapping.items():
+                    if key in key_mapping:
+                        key_mapping[key] += value
+                    else:
+                        key_mapping[key] = value
 
         return FileOperator.write_hashmap_to_file(key_mapping)
