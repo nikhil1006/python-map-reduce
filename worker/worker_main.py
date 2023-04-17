3beda191dc2a03a21fc74e9560fbcbe94625cdcc
Prasanna Nikhil Sathwik Vadlamani <sathwik@Prasannas-MacBook-Pro.local>
2023-04-16 21:37:10 -0400

    used PORT env variable to handle the port issue and for better readability

--- worker/worker_main.py
+++ worker/worker_main.py
@@ -2,6 +2,8 @@ import sys
 import os
 from flask import Flask
 from controller.worker_controller import create_worker_controller
+from flask_cors import CORS
+
 
 
 def main():
@@ -14,9 +16,13 @@ def main():
     print(f"Worker no : {worker_no}")
 
     app = Flask(__name__)
+    CORS(app)
     app.register_blueprint(create_worker_controller(int(worker_no)))
 
-    app.run(port=int(worker_no)+5000)
+    #app.run(port=int(worker_no)+5000)
+    port = int(os.getenv("PORT", 5000))
+    app.run(port=port + int(worker_no))
+
 
 
 if __name__ == "__main__":
